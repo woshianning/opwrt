2c3d72653cc409834299947814bad9a448eb8bab
woshianning <admin@imiliy.cn>
2025-05-25 03:01:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,15 +7,26 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/luci2/bin/config_generat
 mkdir -p files/etc/uci-defaults
 cat <<EOF > files/etc/uci-defaults/99-wifi-enable-ssid
 #!/bin/sh
-uci set wireless.@wifi-device[0].disabled='0'
-uci set wireless.@wifi-iface[0].disabled='0'
-uci set wireless.@wifi-iface[0].ssid='PHICOMM'
-uci set wireless.@wifi-iface[0].key='ikommql45..'
-uci set wireless.@wifi-device[1].disabled='0'
-uci set wireless.@wifi-iface[1].disabled='0'
-uci set wireless.@wifi-iface[1].ssid='PHICOMM'
-uci set wireless.@wifi-iface[1].key='ikommql45..'
-uci commit wireless
+
+# 打开无线设备
+ifconfig ra0 down
+ifconfig rai0 down
+
+# 设置第一个无线接口 SSID 和密码
+iwpriv ra0 set SSID="PHICOMM"
+iwpriv ra0 set AuthMode="WPA2PSK"
+iwpriv ra0 set EncrypType="AES"
+iwpriv ra0 set WPAPSK="ikommql45.."
+iwpriv ra0 set RadioOn=1
+
+# 设置第二个无线接口 SSID 和密码
+iwpriv rai0 set SSID="PHICOMM"
+iwpriv rai0 set AuthMode="WPA2PSK"
+iwpriv rai0 set EncrypType="AES"
+iwpriv rai0 set WPAPSK="ikommql45.."
+iwpriv rai0 set RadioOn=1
+ifconfig ra0 up
+ifconfig rai0 up
 EOF
 
 chmod +x files/etc/uci-defaults/99-wifi-enable-ssid
